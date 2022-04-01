5ad7132cdee576892b37f368d4a713da7ded2793
Joemx <jose.c.mondragon@gmail.com>
2022-03-31 20:49:47 -0600

    Static enemy

---
+++ StaticEnemyController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticEnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        other.gameObject.SendMessage("ReceiveDamage", 0.1);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
