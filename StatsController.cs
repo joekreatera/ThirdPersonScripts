b612ee254e682aade108835a6ef9f5ff1dd04b52
Joemx <jose.c.mondragon@gmail.com>
2022-03-31 20:37:41 -0600

    liife image

---
+++ StatsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsController : MonoBehaviour
+{
+    public Image lifeImage;
+    float life;
+    public float MAX_LIFE = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        life = MAX_LIFE;
+    }
+
+    public void ReceiveDamage(float d) {
+        life = Mathf.Max(0, life - d);
+        float pctLife = life / MAX_LIFE;
+
+        lifeImage.fillAmount = pctLife;
+        lifeImage.color = (Color.green*pctLife  +  Color.red*(1-pctLife));
+    }
+
+}
